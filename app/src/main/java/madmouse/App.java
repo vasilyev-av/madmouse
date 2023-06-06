4a66d7f492cf32e48d53fdba3a09d7df7469ae4a
AVas <jesusv@bk.ru>
2023-06-06 22:53:04 +0500

    Add check if position changed not by app then not move. Add texField to control time between moves.

--- app/src/main/java/madmouse/App.java
+++ app/src/main/java/madmouse/App.java
@@ -4,15 +4,19 @@
 package madmouse;
 
 import java.awt.Frame;
+import java.awt.GridLayout;
 import java.awt.MouseInfo;
 import java.awt.Point;
 import java.awt.AWTException;
 import java.awt.Button;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.Robot;
+import java.awt.TextField;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ThreadLocalRandom;
@@ -24,9 +28,23 @@ public class App {
     public static void main(String[] args) {
         Frame mainFrame = new Frame("MadMouse");
         mainFrame.setSize(100, 100);
+        mainFrame.setLayout(new GridLayout(2, 1));
+        
+        ButtonListener buttonListener = new ButtonListener();
         Button mainButton = new Button("Start");
-        mainButton.addActionListener(new ButtonListener());
+        mainButton.addActionListener(buttonListener);
+        TextField mainText = new TextField("1000");
+        mainText.addTextListener(new TextListener(){
+            @Override
+            public void textValueChanged(TextEvent e) {
+                
+                buttonListener.setPeriod(Integer.parseInt(mainText.getText()));
+            }
+        });
+
+        mainFrame.add(mainText);
         mainFrame.add(mainButton);
+        //mainFrame.pack();
         mainFrame.setVisible(true);
         mainFrame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent we) {
@@ -41,6 +59,12 @@ public class App {
 class ButtonListener implements ActionListener{
     Timer madTimer;
 
+    private int period = 1000;
+
+    public void setPeriod(int jumpTime) {
+        period = jumpTime;
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         Button curButton = (Button) e.getSource();
@@ -48,7 +72,7 @@ class ButtonListener implements ActionListener{
             case "Start":
                 curButton.setLabel("Stop");
                 madTimer = new Timer();
-                madTimer.schedule(new MadMouseMove(), 500, 1000);
+                madTimer.schedule(new MadMouseMove(), period, period);
                 break;
             case "Stop":
                 curButton.setLabel("Start");
@@ -62,6 +86,8 @@ class ButtonListener implements ActionListener{
 class MadMouseMove extends TimerTask{
     Robot madMouse;
 
+    private Point prevPoint = MouseInfo.getPointerInfo().getLocation();
+
     MadMouseMove() {
         try {
             madMouse = new Robot();
@@ -69,9 +95,16 @@ class MadMouseMove extends TimerTask{
             e.printStackTrace();
         }
     }
+
     @Override
     public void run() {
             Point curPoint = MouseInfo.getPointerInfo().getLocation();
-            madMouse.mouseMove(curPoint.x+ThreadLocalRandom.current().nextInt(-5, 6), curPoint.y+ThreadLocalRandom.current().nextInt(-5, 6));
+
+            if (prevPoint.distance(curPoint) == 0) {
+                prevPoint = new Point(curPoint.x+ThreadLocalRandom.current().nextInt(-5, 6), curPoint.y+ThreadLocalRandom.current().nextInt(-5, 6));
+                madMouse.mouseMove(prevPoint.x, prevPoint.y);
+            } else {
+                prevPoint = curPoint;
+            }
     }
 }
